d61ee76a4b7ccf20ad70aeec265f65e70a4fcdbc
ARYA <aryaRbarua@gmail.com>
2020-05-25 20:12:41 +0530

    just started

---
+++ scriptECMA.js
@@ -0,0 +1,30 @@
+//ECMA6
+//ECMASCRIPT === JAVASCRIPT
+// here two assignment are used const and let(better than var but almost same, let is better)
+
+//eg of let and var no difference
+
+// var name = "Abul";
+// let score = "20";
+
+// if (score < 50){
+
+//     console.log(`${name}`,"not valid" ,score);
+// }
+// else{
+
+//     console.log("valid")
+// }
+
+// console.log(score);
+
+
+
+//EcmaScript7
+
+let square = (x)=>x**2
+
+
+console.log(square)
+
+
