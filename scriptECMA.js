10f83d8e8b28f076f1b8a4e1de5738453b43fbf5
ARYA <aryaRbarua@gmail.com>
2020-05-29 07:18:36 +0530

    try

--- scriptECMA.js
+++ scriptECMA.js
@@ -1,30 +1,236 @@
-//ECMA6
-//ECMASCRIPT === JAVASCRIPT
-// here two assignment are used const and let(better than var but almost same, let is better)
+// console.log(square)
 
-//eg of let and var no difference
+// ----- Edabit -----
+  
+    //   Question link : https://edabit.com/challenge/ARr5tA458o2tC9FTN 
 
-// var name = "Abul";
-// let score = "20";
+// Very Easy
 
-// if (score < 50){
+// console.log("hello")
 
-//     console.log(`${name}`,"not valid" ,score);
+
+// var hello = 
+
+
+
+// Object
+
+
+// var a = {
+
+//     name : "john",
+//     age : 38,
+//     hobby : "Soccer"
+// };
+
+// c = a.name
+// console.log(c)
+
+
+// function object(){
+
+//     a = {
+
+//         name : "John",
+//         Address : "NYC",
+//         number : 4485
+//     }
+
+//     console.log(a.Address)
+//     return
 // }
-// else{
 
-//     console.log("valid")
+// object()
+
+//Methods
+
+// var details = {
+
+//     name : "John",
+//     Resident : "NYC",
+//     workplace : "Seattle",
+//     Age : "23",
+
+//     perdetails : function(){
+
+//         return "His name is " + this.name + " and age is " + this.age ;
+//     },
 // }
 
-// console.log(score);
+// document.getElementById("demo").innerHTML = details.perdetails();
+
+// keyless car entry
 
 
 
-//EcmaScript7
+// var checkDriverAge = () =>{
+// var age = prompt("What is your age?");
+// if (Number(age) < 18) {
+// 	alert("Sorry, you are too yound to drive this car. Powering off");
+// } else if (Number(age) > 18) {
+// 	alert("Powering On. Enjoy the ride!");
+// } else if (Number(age) === 18) {
+// 	alert("Congratulations on your first year of driving. Enjoy the ride!");
+// }
 
-let square = (x)=>x**2
+// return 
+// }
+
+// checkDriverAge()
 
 
-console.log(square)
+// Javascript is Sexy
 
+// Objects
+
+// var objects = {
+
+//     firstname : "Arya",
+//     lastname : "barua",
+//     22 : "age"
+
+    
+// }
+
+// console.log(objects.firstname);
+// console.log(objects.lastname);
+// console.log(objects["22"]);
+
+//referwnce datatypes vs Primitive datatypes
+
+//reference datatype
+
+// var name = "Arya"
+// var Sister = name
+// name = "Sansa" 
+
+//console.log(Sister)     ---> here name is Arya
+// console.log(name)      ---> here name is Sansa
+
+//Primitive Datatypes
+
+// var name = {
+
+//     namegot : "Arya"
+// }
+// var Sister = name;
+// name.namegot = "Sansa"
+
+// console.log(name.namegot)     ---> here name is Sansa
+// console.log(Sister.namegot);  ---> here name is Sansa
+
+
+//contructor literals
+
+// var obj = {
+
+//     name : "Arya",
+
+//     name2 : function(){
+//         console.log("Sansa");
+//     }
+// }
+// console.log(obj)
+
+//object constructors for small scale
+
+// var obj = new Object();    //<--- constructor
+
+// obj.name ="Arya";
+// obj.sister = "Sansa";
+
+// obj.brother = function()
+// { console.log("jon snow")};
+
+// console.log(obj)
+// console.log(obj.brother)
+
+
+// pattern for creating Objects
+
+// function Info(Name, Age, AccountNo, bank)
+// {
+//     this.Name = Name;
+//     this.Age = Age;
+//     this.AccountNo = AccountNo;
+//     this.bank = bank;
+
+//     this.statement  = function(){
+//         return "Your "+ this.name + "Age" + this.Age 
+//         +" AccountNo" + this.AccountNo + "and" + " bank" + this.bank
+//     }
+// }
+
+// var personinfo = new Info("Arya" , 21 , 123456 , "SBI");
+
+// console.log(personinfo);
+
+//Accessing Inherited Items
+
+// function School(){
+
+//     this.midSchool = "schoolmid";
+//     this.teacherStrength = "60";
+// }
+
+// var obj = new School()    // using contructor inhriting the property
+
+// obj.name = "vkv";
+// obj.place = "kanyakumari";
+// obj.principal = "savdeykar ji";
+
+// for (var eachItem in obj){
+//    console.log(eachItem;
+// }     
+
+//JSON stringify
+
+
+// var bank = {
+
+//     name : "sbi",
+//     branches : "549",
+//     employees : 80000
+
+// }
+
+// var bank1 = JSON.stringify(bank)  //in JSON stringify the object is converted to as string using JSON.Stringyfy
+// console.log(bank1)
+
+// var bank2 = JSON.parse(bank1)     //in JSON stringify the string is converted to as object using JSON.parse
+// console.log(bank2.name)
+
+
+//PROTOTYPE
+
+// from MDN
+
+// function person(first,last,Age,Height,Chest,Biceps,Waist,Thighs){
+
+//     this.fullName = {
+         
+//         'first' : first,
+//         'last' : last 
+
+//     }
+//     this.Age = Age;
+//     this.Height = Height;
+//     this.bodymeasurement = {
+
+//         'Chest' : Chest,
+//         'Biceps' : Biceps,
+//         'Waist' : Waist,
+//         'Thighs' : Thighs
+//     }
+// }
+
+// var person1 = new person('Ankit','Pathak',24,170,40,15,32,25)
+
+// console.log(person1.bodymeasurement)
+
+
+// using Object.prototype
+
+let person = Object.create(person1);
+person.__proto__
 
